4d82821e40f2956c20bf91c42266b910ea052d10
Crystal <lingchequ@gmail.com>
2023-12-16 16:16:20 +0800

    modify

--- pico9_2.py
+++ pico9_2.py
@@ -6,7 +6,16 @@ def callback1(t):
 def callback2(t):
     print(2)
     
+#time2 = Timer()
+#time2.init(period=2000,callback=callback2)
 
-
-time2 = Timer()
-time2.init(period=2000,callback=callback2)sss
+def run10(t):
+    global i
+    i += 1
+    if i==10:
+        t.deinit()
+    print(i)
+    
+i=1
+timer = Timer(period=1000, mode=Timer.PERIODIC, callback=lambda t:run10(t))
+timer = Timer(period=1000, mode=Timer.PERIODIC, callback=lambda t:print('一直執行'))
