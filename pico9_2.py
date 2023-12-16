5e9d3662b1d62755b2440af83531d38985689f2a
Crystal <lingchequ@gmail.com>
2023-12-16 14:16:35 +0800

    modify

--- pico9_2.py
+++ pico9_2.py
@@ -1,10 +1,12 @@
-from machine import ADC,Timer
+from machine import Timer
 
-def callback1(t:Timer):
-    sensor = ADC(4)    
-    vol = sensor.read_u16() * (3.3/65535)
-    temperature = 27 - (vol-0.706) / 0.001721
-    print(temperature)
+def callback1(t):
+    print(1)
     
-time1 = Timer()
-time1.init(period=1000,callback=callback1)
+def callback2(t):
+    print(2)
+    
+
+
+time2 = Timer()
+time2.init(period=2000,callback=callback2)sss
